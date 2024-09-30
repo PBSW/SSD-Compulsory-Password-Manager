e1ca9bcdcc02ba25a524db0d815cce21745faef2
RSS <90904706+sandbxk@users.noreply.github.com>
2024-09-30 10:30:25 +0200

    Create initial Domain

---
+++ PM-Domain/ServiceCredentials.cs
@@ -0,0 +1,9 @@
+namespace PM_Domain;
+
+public class ServiceCredentials
+{
+    public string ServiceName { get; set; }
+    public string ServiceUsername { get; set; }
+    public string ServicePassword { get; set; }
+    public string IV { get; set; }
+}

15e233bcc39e4859f0ce0d6b34ae34eff1805f18
Philip Zadeh <philezad@gmail.com>
2024-10-08 00:49:03 +0200

    Implemented Argon2 hash for passwords

--- PM-Security/Hasher/PasswordHasher.cs
+++ PM-Security/Hasher/PasswordHasher.cs
@@ -8,6 +8,17 @@ public class PasswordHasher : IPasswordHasher
 {
     public string Hash(string password, string salt)
     {
-        throw new NotImplementedException();
+        byte[] saltByte = Encoding.UTF8.GetBytes("Key" + salt);
+
+        var argon2 = new Argon2id(Encoding.UTF8.GetBytes(password))
+        {
+            Salt = saltByte,
+            DegreeOfParallelism = 4,
+            MemorySize = 65536,
+            Iterations = 4
+        };
+        byte[] hash = argon2.GetBytes(32);
+        
+        return Convert.ToBase64String(hash);
     }
 }
